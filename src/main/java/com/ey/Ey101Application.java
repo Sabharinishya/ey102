5a6cb7d1267264f667243340911a7f3e4a38887b
Sabharinishya <sabharinishya22@gmail.com>
2026-01-07 12:23:56 +0530

    first commit

---
+++ src/main/java/com/ey/Ey101Application.java
@@ -0,0 +1,13 @@
+package com.ey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ey101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ey101Application.class, args);
+	}
+
+}
